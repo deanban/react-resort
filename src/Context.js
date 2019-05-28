464d3c87a2d394e8946766bd3532cc09d0b47a67
deanban <pbp2005@gmail.com>
2019-05-28 15:14:42 -0400

    context is working with data file

--- src/Context.js
+++ src/Context.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
+import items from './data';
 
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
-  state = {};
+  state = {
+    rooms: [],
+    sorted: [],
+    featured: [],
+    loading: true
+  };
+
+  // getData = () => {}
+
+  componentDidMount() {
+    const rooms = this.formatData(items);
+    // console.log(rooms);
+    const featured = rooms.filter(room => room.featured === true);
+
+    this.setState({
+      rooms,
+      featured,
+      sorted: rooms,
+      loading: false
+    });
+  }
+
+  formatData = items => {
+    let tempItems = items.map(item => {
+      const id = item.sys.id;
+      const images = item.fields.images.map(image => image.fields.file.url);
+      let room = { ...item.fields, images, id };
+
+      return room;
+    });
+    return tempItems;
+  };
 
   render() {
     return (
-      <RoomContext.Provider value="hello">
+      <RoomContext.Provider value={{ ...this.state }}>
         {this.props.children}
       </RoomContext.Provider>
     );
